46694db197d0c6ce635ecb8a223049e14ccdffcc
Simeon Andreev <simeon.andreev@softwaregroup.com>
2022-03-30 23:44:33 +0300

    misc: AAA-000 - sample code woo

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,14 @@
+fun String.countDistinctCharacters() = lowercase().toList().distinct().count()
+
 fun main(vararg args: String) {
     println("Hello, Kotlin/Native!")
+    // Read the input value.
+    println("Hello, enter your name:")
+    val name = readLine()
+    // Count the letters in the name.
+    name?.replace(" ", "").let {
+        println("Your name contains ${it?.length} letters")
+        // Print the number of unique letters.
+        println("Your name contains ${it?.countDistinctCharacters()} unique letters")
+    }
 }
